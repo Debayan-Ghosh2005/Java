93d0bdebd3ce968d2f017087cf47669c3505e879
Debayan-Ghosh2005 <2005debayan@gmail.com>
2025-04-30 20:26:26 +0530

    comit1

--- Hexadec_binary.java
+++ Hexadec_binary.java
@@ -1,41 +1,17 @@
-class Hexadec_binary
-{
-    public static void main(String args[])
-    {
-        int n=15;
-        String s="";
-        while(n>0)
-        {
-            int r=n%16;
-            if(r==10)
-            {
-                s='A'+s;
+class Hexadec_binary {
+    public static void main(String args[]) {
+        int n = 15;
+        String s = "";
+        while (n > 0) {
+            int r = n % 16;
+            if (r >= 10) {
+                // Convert to hexadecimal character
+                s = (char) ('A' + (r - 10)) + s; // 'A' corresponds to 10
+            } else {
+                s = r + s; // Concatenate the integer as a string
             }
-            else if(r==11)
-            {
-                s='B'+s;
-            }
-            else if(r==12)
-            {
-                s='C'+s;
-            }
-            else if(r==13)
-            {
-                s='D'+s;
-            }
-            else if(r==14)
-            {
-                s='E'+s;
-            }
-            else if(r==15)
-            {
-                s='F'+s;
-            }
-            else{
-                s=r+s;
-            }
-            n=n/16;
+            n = n / 16;
         }
-        System.out.println("HexaDecimal : "+s);
+        System.out.println("HexaDecimal: " + s);
     }
 }
